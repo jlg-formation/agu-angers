24c2028daa67360115c741c4372d6fc948cf724f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-22 14:24:51 +0200

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { CreateComponent } from './routes/create/create.component';
+import { QuestionsComponent } from './routes/questions/questions.component';
+import { AddQuestionComponent } from './routes/add-question/add-question.component';
+import { SavedComponent } from './routes/saved/saved.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create', component: CreateComponent },
+  { path: 'questions', component: QuestionsComponent },
+  { path: 'add-question', component: AddQuestionComponent },
+  { path: 'saved', component: SavedComponent },
 ];
 
 @NgModule({
