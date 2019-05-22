7b47c443a67e2e12cabd670a10fa14daffce0e28
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-22 17:37:26 +0200

    ok

---
+++ src/app/quizz.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Quizz } from './quizz';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizzService {
+
+  currentQuizz: Quizz;
+
+  create(name: string) {
+    this.currentQuizz = new Quizz(name);
+  }
+
+}
