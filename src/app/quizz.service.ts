5f71a6fab5494c31781eade985e212286de9eb33
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-23 10:24:12 +0200

    ok

--- src/app/quizz.service.ts
+++ src/app/quizz.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Quizz } from './quizz';
 
+const CURRENT = 'current';
+
 @Injectable({
   providedIn: 'root'
 })
 export class QuizzService {
 
+
   currentQuizz: Quizz;
+
+  constructor() {
+    this.retrieveLocal();
+  }
 
   create(name: string) {
     this.currentQuizz = new Quizz(name);
     console.log('quizz', this.currentQuizz);
+    this.saveLocal();
   }
+
+  saveLocal() {
+    localStorage.setItem(CURRENT, JSON.stringify(this.currentQuizz));
+  }
+
+  retrieveLocal() {
+    const quizz = JSON.parse(localStorage.getItem(CURRENT));
+    if (!quizz) {
+      return;
+    }
+    quizz.__proto__ = Quizz.prototype;
+    this.currentQuizz = quizz;
+  }
+
+
+
+
 
 }
