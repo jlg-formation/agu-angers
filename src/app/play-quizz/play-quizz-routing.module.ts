f42bc1f261ffe0919a29120cc0ac11d8b4b1b3ef
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-23 12:13:08 +0200

    ok

--- src/app/play-quizz/play-quizz-routing.module.ts
+++ src/app/play-quizz/play-quizz-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './routes/list/list.component';
+import { StartComponent } from './routes/start/start.component';
+import { AnswerComponent } from './routes/answer/answer.component';
+import { ScoreComponent } from './routes/score/score.component';
 
 const routes: Routes = [
-  { path: 'list', component: ListComponent }
+  { path: 'list', component: ListComponent },
+  { path: 'start', component: StartComponent },
+  { path: 'answer', component: AnswerComponent },
+  { path: 'score', component: ScoreComponent },
 ];
 
 @NgModule({
--- src/app/play-quizz/play-quizz.module.ts
+++ src/app/play-quizz/play-quizz.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { PlayQuizzRoutingModule } from './play-quizz-routing.module';
 import { ListComponent } from './routes/list/list.component';
+import { StartComponent } from './routes/start/start.component';
+import { AnswerComponent } from './routes/answer/answer.component';
+import { ScoreComponent } from './routes/score/score.component';
 
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [ListComponent, StartComponent, AnswerComponent, ScoreComponent],
   imports: [
     CommonModule,
     PlayQuizzRoutingModule
